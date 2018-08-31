811494a0e68c36eda3d6a4b68c3887ebc9b33dbb
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-31 11:13:06 -0500

    update

---
+++ Enrichment/Enrichment/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	double celsius8am;
+	double celsius12pm;
+	double celsius5pm;
+	double fahrenheit8am;
+	double fahrenheit12pm;
+	double fahrenheit5pm;
+
+	cout << "What is the temperature in celsius at 8am ";
+	cin >> celsius8am;
+
+	cout << "What is the temperature in celsius at 12pm ";
+	cin >> celsius12pm;
+
+	cout << "What is the temperature in celsius at 5pm";
+	cin >> celsius5pm;
+
+	fahrenheit8am = celsius8am * 1.8 + 32;
+	fahrenheit12pm = celsius12pm * 1.8 + 32;
+	fahrenheit5pm = celsius5pm * 1.8 + 32;
+
+	cout << "The temperature for fahrenheit 8 am is " << fahrenheit8am << endl;
+	cout << "The temperature for fahrenheit 12 pm is " << fahrenheit12pm << endl;
+	cout << "The temperature for fahrenheit 5 pm is " << fahrenheit5pm << endl;
+
+	system("pause");
+
+	return 0;
+
+
+
+
+
+}
