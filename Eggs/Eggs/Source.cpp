c087bb5d408977bbfca5d533a249a6468f638d46
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-05 08:29:45 -0500

    updated file

---
+++ Eggs/Eggs/Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	double dozen = 2.00;
+	int input;
+	double eggcost = 0.25;
+	double total;
+	int remainder;
+
+	cout << " How many eggs do you like ";
+	cin >> input;
+	{
+		if (input < 12)
+
+			total = input * eggcost;
+
+		cout << "it will cost $ " << total << endl;
+	}
+	{
+	if (input > 12)
+
+	remainder = input / 12;
+	total = remainder + dozen;
+
+	cout << "it will cost $ " << total << endl;
+}
+
+
+
+
+
+
+	system("pause");
+
+	return 0;
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
