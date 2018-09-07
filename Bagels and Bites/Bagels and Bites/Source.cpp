ac8ee8d14627565832e7a3bf6b914205c0261b55
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-07 10:21:48 -0500

    update

--- Bagels and Bites/Bagels and Bites/Source.cpp
+++ Bagels and Bites/Bagels and Bites/Source.cpp
@@ -5,22 +5,37 @@ using namespace std;
 int main()
 {
 
+	int bagels;
+	double bagelsCost;
+	int donuts;
+	double donutsCost;
+	int coffee;
+	double coffeesCost;
+	int totalCost;
+
+	cout << "Enter the a amount bagels: ";
+	cin >> bagels;
 
 
+	cout << "Enter the a amount donuts: ";
+	cin >> donuts;
 
 
+	cout << "Enter the a amount coffees: ";
+	cin >> coffee;
 
+	bagelsCost = bagels * .99;
+	donutsCost = donuts * .75;
+	coffeesCost = coffee * 1.20;
 
+	totalCost = bagelsCost + donutsCost + coffeesCost;
 
-
+	cout << "That will be $ " << totalCost << endl;
 
 
 
 	system("pause");
 
-	return 0
-
-	
-
+	return 0;
 
 }
