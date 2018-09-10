cebec4feed20c3289887e8d9cbff12d4368d31b4
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-10 10:21:47 -0500

    updated file

---
+++ Delgados Taco/Delgados Taco/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	
+	int taco;
+	int burritos;
+	int beans;
+	int tostadas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+
+	return 0;
+}
